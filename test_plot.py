85033cc6151a4fe0bc9e269f55361042074ea193
markus583 <markus.frohmann@gmail.com>
2023-12-29 16:54:19 +0100

    merge changes from unet-baseline

--- test_plot.py
+++ test_plot.py
@@ -1,17 +1,17 @@
 import torch
 import json
+import matplotlib.pyplot as plt
+
 from lightning.pytorch import seed_everything
 from src.model.restoration_module import RestorationLitModule
 from pathlib import Path
 
 # Here is the place to import your stuff
 from src.data.emb_datamodule import EmbeddingDataModule as DataModule
-from src.model.dinov2.simple_decoder import SimpleDecoder as Model
+from src.model.dinov2.conv_decoder import ModifiedConvHead as Model
 
 
 def plot_images(raw, pred, gt):
-    import matplotlib.pyplot as plt
-
     raw = raw * 255
     gt = gt * 255
     # scale prediction values to [0, 255] using min-max scaling
@@ -23,16 +23,35 @@ def plot_images(raw, pred, gt):
     pred = pred.cpu().numpy().astype(int)
     cmap = "gray"
     fig, axs = plt.subplots(1, 3)
-    axs[0].imshow(raw, cmap=cmap)
+    axs[0].imshow(raw.reshape(512, 512), cmap=cmap)
     axs[0].set_title("Input")
-    axs[1].imshow(pred, cmap=cmap)
+    axs[1].imshow(pred.reshape(512, 512), cmap=cmap)
     axs[1].set_title("Prediction")
-    axs[2].imshow(gt, cmap=cmap)
+    axs[2].imshow(gt.reshape(512, 512), cmap=cmap)
     axs[2].set_title("GT")
     plt.tight_layout()
     for ax in axs:
         ax.set_xticks([])
         ax.set_yticks([])
+
+    return fig
+
+
+def plot_pred_image(pred):
+    # scale prediction values to [0, 255] using min-max scaling
+    pred = pred - pred.min()
+    pred = pred / pred.max()
+    pred = pred * 255
+    pred = pred.cpu().numpy().astype(int)
+    cmap = "gray"
+
+    # Setting figure size to match the image size
+    # XXX: this assumes images are 512x512!
+    fig_size = 512 / 80  # Convert pixels to inches for matplotlib (80 dpi is default)
+    fig = plt.figure(figsize=(fig_size, fig_size), dpi=80, frameon=False)
+    ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frame_on=False)
+    ax.imshow(pred.reshape(512, 512), cmap=cmap)
+    ax.axis("off")
 
     return fig
 
@@ -46,7 +65,7 @@ def main():
         data_paths = json.load(file)
 
     # load model
-    net = Model()
+    net = Model(in_channels=768)
     net.eval()
     # load data
     data_module = DataModule(data_paths_json_path=data_paths_json_path)
@@ -55,7 +74,11 @@ def main():
     pl_module = RestorationLitModule(
         optimizer=None, scheduler=None, compile=None, encoder=net
     )
+<<<<<<< HEAD
     checkpoint_path = "CV2023/al7538jh/checkpoints/last.ckpt"
+=======
+    checkpoint_path = "CV2023/v8d05buj/checkpoints/epoch=7-step=22000.ckpt"
+>>>>>>> unet-baseline
     checkpoint = torch.load(checkpoint_path)
     pl_module.load_state_dict(checkpoint["state_dict"])
 
@@ -75,7 +98,3 @@ def main():
             .joinpath(f"image_{idx}_test.png")
         )
         fig.show()
-
-
-if __name__ == "__main__":
-    main()
