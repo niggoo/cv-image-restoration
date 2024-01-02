86d8d5263f6100b3fecbbc5a4e0ef60ec1ab8f10
noah <noah-pichler@gmx.at>
2024-01-02 18:15:36 +0100

    dinov2 and dpt

---
+++ src/model/dinov2/dinov2.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torchinfo
+from transformers import Dinov2Model, Dinov2Config
+
+
+class Dinov2 (nn.Module):
+    def __init__(self, dinov2_size: str = "small", out_features: list = [4, 8, 12], channels: int = 4) -> None:
+        """Dinov2 model with a selection of output features
+
+        :params dinov2_size: size of the model, either "small", "base", "large" or "giant"
+        """
+        super(Dinov2, self).__init__()
+        model_name = f"facebook/dinov2-{dinov2_size}"
+        config = Dinov2Config.from_pretrained(model_name)
+        # overwrite settings
+        config.num_channels = channels
+        config.patch_size = 16
+        config.output_hidden_states = True
+        self.out_features = out_features
+        self.config = config
+        # init dinov2 model
+        self.model = Dinov2Model.from_pretrained(model_name, config=config, ignore_mismatched_sizes=True)
+        # freeze encoder but leave embeddings trainable
+        for param in self.model.encoder.parameters():  
+            param.requires_grad = False
+
+    def forward(self, x):
+        """
+        Takes a picture in the form (B, IN_C, W, H) and
+        returns a stack of features in the form of ((B, H, W/P, H/P), CLS)
+        :param x: batch of pictures
+        :return: stack of features
+        """
+        # save shape for later
+        B, _, w, h = x.shape
+        # get features and select the relevant stages
+        x = self.model(x)
+        # reformat output
+        outputs = list(x.hidden_states[i] for i in self.out_features)
+        outputs = [(out[:, 1 + 0:], out[:, 0]) for out in outputs]  # zero is the number of register (should be always 0)
+        outputs = [(
+            seq.reshape(B, w // self.config.patch_size, h // self.config.patch_size, -1).permute(0, 3, 1, 2).contiguous(),
+            cls)
+            for seq, cls in outputs
+        ]
+        return outputs
+    
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    module = Dinov2(dinov2_size="small").to(device)
+    x = torch.rand((8, 4, 512, 512)).to(device)
+    torchinfo.summary(module, x.shape)
+    x = module(x)
+    for i in x:
+        print(i[0].shape, i[1].shape)
--- test_train.py
+++ test_train.py
@@ -6,87 +6,35 @@ import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch import Trainer, seed_everything
-from torchinfo import summary
 
-import os
 from argparse import ArgumentParser
 
 from src.model.restoration_module import RestorationLitModule
-
 # Here is the place to import your stuff
-from src.data.emb_datamodule import EmbeddingDataModule as DataModule
-from src.model.dinov2.conv_decoder import ModifiedConvHead as Model
-
-# TODO: we should not do this here :D
-from lightning.pytorch.callbacks import Callback
-import matplotlib.pyplot as plt
-from test_plot import plot_images
-
-
-class ImageLoggingCallback(Callback):
-    def __init__(self, datamodule, num_samples=5, wandb_logger=None):
-        self.datamodule = datamodule
-        self.num_samples = num_samples
-        self.wandb_logger = wandb_logger
-        # save fig to wandb_logger.save_dir
-        self.folder_path = os.path.join(
-            self.wandb_logger._project, self.wandb_logger.version
-        )
-        # create dir
-        if not os.path.exists(self.folder_path):
-            os.makedirs(self.folder_path)
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        pl_module.eval()
-        with torch.no_grad():
-            for idx in range(self.num_samples):
-                embeddings, gt, raw, _ = self.datamodule.data_val.get_all(idx)
-                # print pl module device
-                embeddings = embeddings.to(pl_module.device)
-                # add batch dimension
-                pred = pl_module(embeddings.unsqueeze(0))
-                pred = pred.squeeze(0).detach()
-
-                fig = plot_images(raw=raw, pred=pred, gt=gt)
-                # add filename
-                fig_path = os.path.join(self.folder_path, f"val_image_{idx}.png")
-                # save fig
-                fig.savefig(fig_path)
-
-                # Log the image to wandb
-                try:
-                    self.wandb_logger.log_image(
-                        key=f"val_image_{idx}", images=[fig_path]
-                    )
-                except Exception as e:
-                    # sometimes, there are weird errors
-                    print(e)
-                plt.close(fig)
-
-        pl_module.train()
+from src.data.image_datamodule import ImageDataModule as DataModule
+from src.model.dinov2.dinov2 import Dinov2
+from src.model.dinov2.dpt import DPT
 
 
 def opts_parser():
     usage = "Restores air-images of person in a forest"
     parser = ArgumentParser(description=usage)
     # Training parameters
-    parser.add_argument("--batch_size", type=int, default=8)
-    parser.add_argument("--grad_accum_steps", type=int, default=1)
-    parser.add_argument("--n_epochs", type=int, default=150)
-    parser.add_argument("--max_grad_norm", type=float, default=0.0)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--n_epochs', type=int, default=150)
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--max_grad_norm', type=float, default=0.)
     # learning rate + schedule
-    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
     # Data parameters
-    # parser.add_argument("--data_dirc", type=str)
+    parser.add_argument('--data_dirc', type=str)
     # Model parameters
     parser.add_argument("--d_model", type=int, default=768)
-    # parser.add_argument("--n_layers", type=int, default=12)
+    parser.add_argument('--n_layers', type=int, default=12)
     # Misc.
     parser.add_argument("--num_workers", type=int, default=0)
-    parser.add_argument("--experiment_name", type=str, default="Test")
-    parser.add_argument("--checkpoint", type=str, default=None)
-    parser.add_argument("--test", action="store_true", default=False)
-    parser.add_argument("--pin_memory", action="store_true", default=False)
+    parser.add_argument('--experiment_name', type=str, default="Test")
+    parser.add_argument('--checkpoint', type=str, default=None)
 
     return parser
 
@@ -107,60 +55,36 @@ def main():
         group="Test",
         notes="First Tests",
         config=config,  # this logs all hyperparameters for us
-        name=config.experiment_name,
+        name=config.experiment_name
     )
 
     # training dataset loading
-    datamodule = DataModule(
-        data_paths_json_path="src/data/data_paths_base.json",
-        batch_size=config.batch_size,
-        num_workers=config.num_workers,
-        pin_memory=config.pin_memory,
-    )
+    datamodule = DataModule()
     # create pytorch lightening module
-    net = Model(in_channels=config.d_model)
-    print(summary(net))
-
-    pl_module = RestorationLitModule(
-        optimizer=AdamW,
-        scheduler=ReduceLROnPlateau,
-        compile=False,
-        encoder=net,
-    )
+    enc = Dinov2(dinov2_size="small")  # here is the place to init the model(s)
+    dec = DPT(embed_dims=384, channels=48, post_process_channels=[48, 96, 192, 384])
+    pl_module = RestorationLitModule(optimizer=AdamW, scheduler=ReduceLROnPlateau, compile=False, encoder=enc, decoder=dec)
     # create monitor to keep track of learning rate - we want to check the behaviour of our learning rate schedule
-    lr_monitor = LearningRateMonitor(logging_interval="epoch")
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
     # sets Checkpointing dependency to loss -> we keep the best 2 model according to loss
-    checkpoint_callback = ModelCheckpoint(
-        monitor="val/loss", every_n_epochs=1, save_last=True, save_top_k=1
-    )
-
-    image_logging_callback = ImageLoggingCallback(
-        datamodule, num_samples=100, wandb_logger=wandb_logger
-    )
+    checkpoint_callback = ModelCheckpoint(monitor="train/loss", every_n_train_steps=1000, save_last=True, save_top_k=-1)
     # create the pytorch lightening trainer by specifying the number of epochs to train, the logger,
     # on which kind of device(s) to train and possible callbacks as well as set up for Mixed Precision
-    trainer = L.Trainer(
-        max_epochs=config.n_epochs,
-        logger=wandb_logger,
-        accelerator=device,
-        callbacks=[lr_monitor, checkpoint_callback, image_logging_callback],
-        default_root_dir=None,  # change dirc if needed
-        precision="32",  # 32 is full precision, maybe we need
-        gradient_clip_val=config.max_grad_norm,  # 0. means no clipping
-        accumulate_grad_batches=config.grad_accum_steps,
-        log_every_n_steps=50,
-        devices=-1,
-    )
+    trainer = L.Trainer(max_epochs=config.n_epochs,
+                        logger=wandb_logger,
+                        accelerator=device,
+                        callbacks=[lr_monitor, checkpoint_callback],
+                        default_root_dir=None,  # change dirc if needed
+                        precision="32",  # 32 is full precision, maybe we need
+                        gradient_clip_val=config.max_grad_norm,  # 0. means no clipping
+                        accumulate_grad_batches=1,
+                        log_every_n_steps=50)
     # start training
     trainer.fit(pl_module, datamodule=datamodule, ckpt_path=config.checkpoint)
 
-    # TODO: optionally, test the model
-    # but how to do this best with lightning?
-
-
 if __name__ == "__main__":
     main()
-"""
+'''
 
 sample = next(iter(train_dl)).to("cuda").half()
 pl_module.to("cuda")
@@ -173,4 +97,4 @@ for i in tqdm(range(5000)):
     loss = torch.nn.functional.l1_loss(noise, predicted.squeeze(1))
     loss.backward()
     optimizer.step()
-"""
+'''
